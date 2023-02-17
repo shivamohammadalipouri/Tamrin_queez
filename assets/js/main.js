c3066aacaa34e486cdc9d2c5b5c8417eb6e5631d
shivamohammadalipouri <89724104+shivamohammadalipouri@users.noreply.github.com>
2023-02-17 18:07:33 +0330

    seconds

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,24 +10,43 @@ async function setqueez(){
     .then((res) => res.json())
     .then((json) => {
 
+        // get all queez 
         arr = json.results;
 
         function start(){
 
-            // console.log(arr);
+            // clear question option before 
+            let oldOption = document.getElementsByClassName("option");
+            const oldOption_size = oldOption.length;
+
+            for(let x=0; x<oldOption_size; x++){
+                
+                oldOption[0].remove();
+            }
+
+            // show question 
+            console.log(arr);
             queez.innerText = arr[i].question;
 
-            let answers = arr[i].incorrect_answers;
-            answers.push(arr[i].correct_answer);
-            // console.log(answers);    
-
-            for(let j=0; j<answers.length; j++){
+            // show options 
+            for(let j=0; j<arr[i].incorrect_answers.length+1; j++){
 
                 let optionDiv = document.createElement("div");
                 optionDiv.className = "option";
-                optionDiv.innerText = arr[i].incorrect_answers[j];
-                answer.appendChild(optionDiv);
 
+                if(j==arr[i].incorrect_answers.length){
+
+                    optionDiv.innerText = arr[i].correct_answer;
+                    answer.appendChild(optionDiv);
+                }
+                else{
+
+                    optionDiv.innerText = arr[i].incorrect_answers[j];
+                    answer.appendChild(optionDiv);
+                }
+
+
+                // change the colrs of the circles and change the question 
                 optionDiv.addEventListener("click", (event) => {
 
                     let correct_answer = arr[i].correct_answer;
@@ -35,16 +54,21 @@ async function setqueez(){
 
                     if ( correct_answer == user_answer){
 
-                        let circle_name = "circle_"+(i+1);
+                        let number_circle = i+1;
+                        let circle_name = "circle_"+number_circle;
                         const circle = document.getElementById(circle_name);
                         circle.style.backgroundColor = 'green';
-                    }
 
+                        nextLevel();
+                    }
                     else{
 
-                        let circle_name = "circle_"+(i+1);
+                        let number_circle = i+1;
+                        let circle_name = "circle_"+number_circle;
                         const circle = document.getElementById(circle_name);
                         circle.style.backgroundColor = 'red';
+
+                        nextLevel();
                     }
     
                 });
@@ -52,8 +76,31 @@ async function setqueez(){
             }
         }
         start();
+
+        // update to replace the question 
+        function nextLevel(){
+
+            if (i==arr.length-1){
+
+                i=0;
+
+                // clear the colors of the circles
+                for(let y=1; y<=4; y++){
+
+                    let circle_name = "circle_"+y;
+                    const circle = document.getElementById(circle_name);
+                    circle.style.backgroundColor = 'white';
+                }
+
+            }
+            else{
+                i=i+1;
+            }
+            
+            start();
+
+        }
     })
 }
 
 setqueez();
-
